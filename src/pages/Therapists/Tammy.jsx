573c5f41b5ca5e5a1751bd8835103d073eef0bae
James Kelly <jk377y@gmail.com>
2023-05-20 16:32:58 -0500

    completed tammy component

--- src/pages/Therapists/Tammy.jsx
+++ src/pages/Therapists/Tammy.jsx
@@ -1,8 +1,55 @@
+import IMAGES from "../../assets/images/index";
 
 const Tammy = () => {
   return (
-    <div>Tammy</div>
-  )
+		<>
+		
+		
+			<div className="therapist-container">
+				<div className="therapist-info">
+					
+					<div className="therapist-profile-pic">
+						<img src={IMAGES.tammy} alt="Anna" />
+					</div>
+					<br/>
+					<div className="therapist-info-text">
+						<div className="therapist-logo-title">
+							<img src={IMAGES.happyCreature} alt="happy creature pic" />
+							<div className="therapist-title">
+								<h2>Tammy Davis, LCSW</h2>
+								<h3>Contact</h3>
+								<h3>at</h3>
+								<h3>979-398-2745</h3>
+							</div>
+						</div>
+						<br/>
+						<div className="therapist-education">
+							<h3>Education:</h3>
+							<p>Universtiy of Houston 2016</p>
+							<p>Masters Degree - Social Work 1984</p>
+							<p>Master Social Work</p>
+							<br/>
+							<p>Texas A&M University 2006</p>
+							<p>Master of Public Health</p>
+							<br/>
+							<p>Stephen F. Austin State Universtiy 1996</p>
+							<p>BS Heath Science</p>
+							<br/>
+						</div>
+					</div>
+				</div>
+					<br/>
+					<br/>
+				<div className="therapist-specializations">
+					<p>&nbsp;&nbsp;&nbsp;<span className="therapists-greeting-text">Hi!</span> I am a Licensed Clinical Social Worker with over 20 years experience as an MA, CNA and 6 years of experience meeting the needs of clients in social work settings who struggle with depression, anxiety, addictions, trauma, and family relationships. I specialize in working with single moms with children and those with life event adjustments. As a licensed social worker, I utilize a solution-focused approach along with cognitive and dialectical behavioral therapeutic techniques to uncover, process and assist my clients with managing unwanted symptoms that are keeping you from realizing your personal goals in life. I have worked with individuals, couples, and families in various psychotherapy settings as a clinician. My overall approach includes compassion, empathy, and a deep desire to ensuring my clients have a safe environment to experience positive life changes and live a fulfilling life. I also like to meet clients &quot;where they are at,&quot; and at a pace that works best for them in their life&apos;s journey. I look forward to meeting and working with you. See you Soon!</p>
+					<br/>
+					<br/>
+				</div>
+				<br/>
+				<br/>
+			</div>
+		</>
+	);
 }
 
 export default Tammy
--- src/pages/Therapists/Teresa.jsx
+++ src/pages/Therapists/Teresa.jsx
@@ -40,7 +40,7 @@ const Teresa = () => {
 				<div className="therapist-specializations">
 					<p>Being a Marriage and Family Therapist, I help individuals, couples and families enhance their strengths and scaffold weakness to build and reinforce the resilience and insight necessary for purposeful change. Depression, Anxiety and Stress, Addiction, PTSD and Grief should not lead to hopelessness. I have several years of experience treating children and adolescence. I offer parenting support and work well with children and adolescents, ages 5 through 17. We meet independently and with the family to improve behaviors through cognitive insights that support behavior modifications/changes for ADHD, conduct concerns and social skills development that can be useful in social/sporting, school and home settings. I believe all of us have incredible strengths within but sometimes may need a little guidance connecting to them.</p>
 					<br/>
-					<p>For more information on methods I support to facilitate emotional health and sense of well-being visit www.isftherapy.com. You are the "expert" in your life and with positive decisions and support the change you want to make can be attained. There are possibilities to explore. It would be my privilege to be a part of your possibilities.</p>
+					<p>For more information on methods I support to facilitate emotional health and sense of well-being visit <a href="http://www.isftherapy.com" target="_blank" className="therapist-url-link" rel="noreferrer">www.isftherapy.com</a>. You are the &quot;expert&quot; in your life and with positive decisions and support the change you want to make can be attained. There are possibilities to explore. It would be my privilege to be a part of your possibilities.</p>
 					<br/>
 					<br/>
 				</div>
