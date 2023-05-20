68450d67c51e2f22e25ceeabaa680197f9095eca
James Kelly <jk377y@gmail.com>
2023-05-20 15:46:07 -0500

    completed Anna and Desire components

--- src/components/Therapists/Anna.jsx
+++ src/components/Therapists/Anna.jsx
@@ -5,32 +5,74 @@ const Anna = () => {
 		<>
 		
 		
-		
-			<div className="therapist-info">
-				
-				<div className="therapist-profile-pic">
-					<img src={IMAGES.anna} alt="Anna" />
-				</div>
-				<div className="therapist-info-text">
-					<div className="therapist-logo-title">
-						<img src={IMAGES.happyCreature} alt="happy creature pic" />
-						<div className="therapist-title">
-							<h2>Anna Leason, LCSW</h2>
-							<h3>Contact Anna @ 713-303-9627</h3>
+			<div className="therapist-container">
+				<div className="therapist-info">
+					
+					<div className="therapist-profile-pic">
+						<img src={IMAGES.anna} alt="Anna" />
+					</div>
+					<br/>
+					<div className="therapist-info-text">
+						<div className="therapist-logo-title">
+							<img src={IMAGES.happyCreature} alt="happy creature pic" />
+							<div className="therapist-title">
+								<h2>Anna Leason, LCSW</h2>
+								<h3>Contact</h3>
+								<h3>at</h3>
+								<h3>713-303-9627</h3>
+							</div>
+						</div>
+						<br/>
+						<div className="therapist-education">
+							<h3>Education:</h3>
+							<p>The University of Texas - Austin</p>
+							<p>Masters Degree - Social Work 1984</p>
+							<p>Clinical Hypnosis</p>
+							<br/>
+							<h3>Post Graduate Training:</h3>
+							<p>EMDR (Eye Movement Desensitization and Reprocessing)</p>
+							<p>Mindfulness Oriented Therapy</p>
+							<p>CBT (Cognitive Behavioral Therapy)</p>
+							<p>DBT (Dialectical Behavior Therapy)</p>
+							<p>Neuropsychpharmacology</p>
+							<br/>
 						</div>
 					</div>
-						
-			
-					
-					
-					
 				</div>
+					<br/>
+					<br/>
+				<div className="therapist-specializations">
+					<h3>Anna specializes in:</h3>
+					<br/>
+					<br/>
+					<h4>Anxiety Issues --</h4>
+					<br/>
+					<h5>Obsessive Compulsive Disorder - Post Traumatic Stress Disorder - Panic Attack - Phobias - Worry - Fear - Feeling on Edge</h5>
+					<p>Anxiety has this incredible ability to show up in many shapes and forms. It might be an overwhelming sense of dread or gloom and doom, but not really knowing why you feel that way.It could appear as a panic attack when you might think you are dying or having a heart attack. Sometimes it surfaces in PTSD when you think you are right back in a traumatic situation. Anxiety might work its way in by having you obsess on the same thought over and over and over. Or it could simply be on going worry. Anxiety can make us feel like we are somehow crazy or out of control. The positive aspect of anxiety no matter how it might surface is, it is very treatable. Cognitive/behavioral therapy, insight oriented therapy, mindfulness therapy, hypnosis and EMDR can all be helpful with the treatment of anxiety and help an individual experience more joy without always feeling that other shoe is about to fall.</p>
+					<br/>
+					<br/>
+					<h4>Depression Issues --</h4>
+					<br/>
+					<h5>Sadness - Lack of Joy - Irritability - Difficulty Motivating Oneself - Self Esteem - Job Loss - Death of a loved one - Divorce</h5>
+					<p>Depression has this uncanny ability of robbing you of the joy in life. You find that you no longer enjoy doing the things that you used to. It might be a huge effort to get out of bed and get dressed. You might feel an overwhelming sense of sadness and isolation. Brain fog and difficulty with concentration are common. You might start to notice everything seems to make you feel agitated or irritable. Depression might surface in making you feel you have no purpose or question your value. It can steal your sense of self confidence. With help …sometimes therapy or a combination of medication and therapy you can regain the beautiful essence of who you are and experience the incredible gift of life once again.</p>
+					<br/>
+					<br/>
+				</div>
+				<div className="therapist-treatments">
+					<h4>Treatments Offered--</h4>
+					<br/>
+					<h5>EMDR - Eye Movement Desensitation Reprocessing</h5>
+					<h5>Hypnosis</h5>
+					<h5>CBT - Cognitive Behavioral Therapy</h5>
+					<h5>DBT - Dialectical Behavior Therapy</h5>
+					<h5>Psychodynamics</h5>
+					<h5>Mindfulness Oriented Therapy</h5>
+				</div>
+				<br/>
+				<br/>
+				<br/>
+				<br/>
 			</div>
-		
-		
-			
-		
-		
 		</>
 	);
 };
--- src/components/Therapists/Desire.jsx
+++ src/components/Therapists/Desire.jsx
@@ -1,7 +1,70 @@
+import IMAGES from "../../assets/images/index";
 
 const Desire = () => {
   return (
-    <div>Desire</div>
+    <>
+		
+		
+			<div className="therapist-container">
+				<div className="therapist-info">
+					
+					<div className="therapist-profile-pic">
+						<img src={IMAGES.desire} alt="Anna" />
+					</div>
+					<br/>
+					<div className="therapist-info-text">
+						<div className="therapist-logo-title">
+							<img src={IMAGES.happyCreature} alt="happy creature pic" />
+							<div className="therapist-title">
+								<h2>Desiré Martinez, LCSW-S</h2>
+								<h3>Contact</h3>
+								<h3>at</h3>
+								<h3>281.671.7073</h3>
+							</div>
+						</div>
+						<br/>
+						<div className="therapist-education">
+							<h3>Education:</h3>
+							<p>University of Houston Graduate College of Social Work</p>
+							<p>Masters Degree - Social Work - 2010</p>
+							<p>Post Graduate Training: Traumatic Stress Studies, CBT, DBT, IFS</p>
+							<br/>
+						</div>
+					</div>
+				</div>
+					<br/>
+					<br/>
+				<div className="therapist-specializations">
+					<p>Whether you are dealing with a life-long issue (low self esteem, trauma, depression, anxiety) or dealing with phase of life problems (parenting, grief/loss, relationship challenges, work/school stress, chronic medical issues) therapy can help you identify self-limiting patterns and learn new skills to manage thoughts, feelings and behaviors. If you are ready to change the way you are feeling, I am ready to work through it with you. I offer culturally sensitive counseling services, in both English and Spanish and welcome all ethnicities, abilities, cultural backgrounds, and sexual/gender identities. While my passion is focusing on self-esteem, self-acceptance, resiliency and posttraumatic growth, my areas of specialty include: Anxiety, Depression, working with survivors of intimate partner violence, survivors of childhood trauma, and complex trauma. I am an LCSW-S and available for clinical supervision as well.</p>
+					<br/>
+					<h5>I offer psychosocial evaluations for immigration cases for use in the following situations:</h5>
+					<br/>
+					<ul>
+						<li>Hardship Waivers (I-601 and I-601A)</li>
+						<li>Cancellation of Removal</li>
+						<li>VAWA and Spousal Abuse</li>
+						<li>U-Visas Asylum, Withholding of Removal, and Convention Against Torture</li>
+					</ul>
+					<br/>
+					<p>Ya sea que esté lidiando con un problema de por vida (baja autoestima, trauma, depresión, ansiedad) o lidiando con problemas de una fase de su vida (crianza, duelo / pérdida, problemas de pareja, estrés laboral / escolar, problemas médicos crónicos), la terapia puede ayudarlo(a) a identificar patrones que lo(a) limitan y aprender nuevas habilidades para manejar sus pensamientos, sentimientos y comportamientos. Si está listo(a) para cambiar la forma en que se siente, estoy lista para trabajar usted. Ofrezco servicios de consejería culturalmente sensibles, tanto en inglés como en español y doy la bienvenida a todas las etnicidades, habilidades, antecedentes culturales e identidades sexuales / de género. Aunque mi pasión se centra en la autoestima, la autoaceptación, la resiliencia y el crecimiento postraumático, mis áreas de especialidad incluyen: Ansiedad, Depresión, trabajo con sobrevivientes de violencia doméstica, sobrevivientes de trauma infantil y trauma complejo. También soy LCSW-S y estoy disponible para supervisión clínica. </p>
+					<br/>
+					<h5>Ofrezco evaluaciones psicosociales para casos de inmigración para su uso en las siguientes situaciones:</h5>
+					<br/>
+					<ul>
+						<li>Exenciones por dificultades (I-601 e I-601A)</li>
+						<li>Cancelación de la eliminación</li>
+						<li>VAWA y abuso conyugal</li>
+						<li>Visas</li>
+						<li>Asilo, retención de expulsión y Convención contra la Tortura</li>
+					</ul>
+					<br/>
+					<br/>
+					<br/>
+				</div>
+				<br/>
+				<br/>
+			</div>
+		</>
   )
 }
 
