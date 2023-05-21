bfde02e0f2a482b66be8c9ecf0c60184c766164a
James Kelly <jk377y@gmail.com>
2023-05-21 04:35:08 -0500

    changed import path of header image for deployment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
-import header2a from "../assets/images/index/header-2a.png";
+import IMAGES from "../assets/images";
 import Navbar from "./Navbar";
 
 const Header = () => {
 	return (
 		<header className="header">
-			<a href="/"><img className="header-image" src={header2a} alt="header2a image"></img></a>
+			<a href="/"><img className="header-image" src={IMAGES.headerLogo} alt="header2a image"></img></a>
 			<Navbar />
 		</header>
 	);
