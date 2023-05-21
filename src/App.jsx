d069f8a88b031beb39e4f0b1a4b27c31a4f80634
James Kelly <jk377y@gmail.com>
2023-05-21 04:28:41 -0500

    changed case sensitive naming

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/header";
+import Header from "./components/Header";
 import MobileHamburgerNav from "./components/MobileHamburgerNav";
 import Homepage from "./pages/Homepage";
 import OurTherapists from "./pages/OurTherapists";
