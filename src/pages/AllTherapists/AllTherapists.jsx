0fd0e107be9e95d6fe5abb2a9cf50853affbf294
James Kelly <jk377y@gmail.com>
2023-05-25 13:12:44 -0500

    changed color theme

--- src/pages/AllTherapists/AllTherapists.jsx
+++ src/pages/AllTherapists/AllTherapists.jsx
@@ -5,34 +5,34 @@ import "./AllTherapists.css";
 const AllTherapists = () => {
 	return (
 		<>
-			<div className="all-therapists-pics-container">
-				<div className="all-therapists-pics-scroll-view">
-					<div className="all-therapists-anna-container">
+			<div className="all-therapists-outer-container">
+				<div className="all-therapists-inner-container">
+					<div>
 						<p>Anna Leason,</p>
 						<p>LCSW</p>
 						<Link to="/therapists/anna"><img src={IMAGES.anna} alt="Anna" loading="lazy" className="all-therapists-pics"/></Link>
 					</div>
-					<div className="all-therapists-teresa-container">
+					<div>
 						<p>Teresa Linnemann,</p>
 						<p>LMFT</p>
 						<Link to="/therapists/teresa"><img src={IMAGES.teresa} alt="Teresa" loading="lazy" className="all-therapists-pics"/></Link>
 					</div>
-					<div className="all-therapists-leann-container">
+					<div>
 						<p>Leann Jenkins,</p>
 						<p>LPC Associate</p>
 						<Link to="/therapists/leann"><img src={IMAGES.leann} alt="Leann" loading="lazy" className="all-therapists-pics"/></Link>
 					</div>
-					<div className="all-therapists-desire-container">
+					<div>
 						<p>Desiré Martinez,</p>
 						<p>LCSW-S</p>
 						<Link to="/therapists/desire"><img src={IMAGES.desire} alt="Desire" loading="lazy" className="all-therapists-pics"/></Link>
 					</div>
-					<div className="all-therapists-tammy-container">
+					<div>
 						<p>Tammy Davis,</p>
 						<p>LCSW</p>
 						<Link to="/therapists/tammy"><img src={IMAGES.tammy} alt="Tammy" loading="lazy" className="all-therapists-pics"/></Link>
 					</div>
-					<div className="all-therapists-reneka-container">
+					<div>
 						<p>Reneka Clark,</p>
 						<p>LCSW</p>
 						<Link to="/therapists/reneka"><img src={IMAGES.reneka} alt="Reneka" loading="lazy" className="all-therapists-pics"/></Link>
