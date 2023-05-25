be97b51e8fca5a89810afefd0ddd7747514b9a88
James Kelly <jk377y@gmail.com>
2023-05-25 01:04:14 -0500

    moved animation on mobile nav back to enter from the right side

--- src/components/MobileHamburgerNav/MobileHamburgerNav.jsx
+++ src/components/MobileHamburgerNav/MobileHamburgerNav.jsx
@@ -5,36 +5,37 @@ import "./MobileHamburgerNav.css";
 
 const MobileHamburgerNav = () => {
 	const [isOpen, setIsOpen] = useState(false);
-    const toggleNav = () => {
-        setIsOpen(!isOpen);
-        const mobileMenuDiv = document.querySelector("#nav-mobile-menu-div");
-        if (!isOpen) {
-			mobileMenuDiv.classList.add("show");
-            mobileMenuDiv.classList.remove("hide");
-		} else {
-			mobileMenuDiv.classList.remove("show");
-            mobileMenuDiv.classList.add("hide");
-		}
-    };
+
+	const toggleNav = () => {
+		setIsOpen(!isOpen);
+	};
+
 	return (
-		<>
-			<div className="nav-mobile-outer-div shadow-frame">
-				<button className={`hamburgerBtn ${isOpen ? "collapsed" : ""}`} aria-label="Toggle Navigation" onClick={toggleNav} >
-					<span className="hamburger"><span></span><span></span><span></span></span></button>
-				<div id='nav-mobile-menu-div' className={`nav-mobile-menu-div ${isOpen ? "show" : "hide"}`}>
-					<div className="nav-mobile-menu">
-						<div>
-							<img src={IMAGES.headerLogo} alt="logo" />
-						</div>
-						<Link to="/">HOME</Link>
-						<Link to="/therapists">OUR TEAM</Link>
-						<Link to="/office">OUR OFFICE</Link>
-						<Link to="/information">INFORMATION</Link>
-						<Link to="/contact">CONTACT US</Link>
+		<div className="nav-mobile-outer-div shadow-frame">
+			<button
+				className={`hamburgerBtn ${isOpen ? "collapsed" : ""}`}
+				aria-label="Toggle Navigation"
+				onClick={toggleNav}
+			>
+				<span className="hamburger"></span>
+			</button>
+			<div
+				id="nav-mobile-menu-div"
+				className={`nav-mobile-menu-div ${isOpen ? "show" : ""}`}
+			>
+				<div className="nav-mobile-menu">
+					<div>
+						<img src={IMAGES.headerLogo} alt="logo" />
 					</div>
+					<Link to="/" onClick={toggleNav}>HOME</Link>
+					<Link to="/therapists" onClick={toggleNav}>OUR TEAM</Link>
+					<Link to="/office" onClick={toggleNav}>OUR OFFICE</Link>
+					<Link to="/information" onClick={toggleNav}>INFORMATION</Link>
+					<Link to="/contact" onClick={toggleNav}>CONTACT US</Link>
 				</div>
 			</div>
-		</>
-	)
-}
+		</div>
+	);
+};
+
 export default MobileHamburgerNav;
