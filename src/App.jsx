33b3d468863b8f31aba15026d48d26e6818f4a85
James Kelly <jk377y@gmail.com>
2023-06-11 06:37:18 -0500

    minor gramatical changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import MobileHamburgerNav from "./components/MobileHamburgerNav/MobileHamburgerNav";
 import Homepage from "./pages/Homepage/Homepage";
